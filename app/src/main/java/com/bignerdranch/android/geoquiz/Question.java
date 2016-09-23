679537d8af8fd9ed1f93aa263a887eceaf70c5c5
SymonLuo <symon_luo@126.com>
2016-09-23 21:17:52 +0800

    add class

---
+++ app/src/main/java/com/bignerdranch/android/geoquiz/Question.java
@@ -0,0 +1,33 @@
+package com.bignerdranch.android.geoquiz;
+
+/**
+ * Created by Administrator on 2016/9/23.
+ */
+
+public class Question {
+
+    private int mTextResId;
+
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getmTextResId() {
+        return mTextResId;
+    }
+
+    public void setmTextResId(int mTextResId) {
+        this.mTextResId = mTextResId;
+    }
+
+    public boolean ismAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setmAnswerTrue(boolean mAnswerTrue) {
+        this.mAnswerTrue = mAnswerTrue;
+    }
+}
